f8da066af802e41662e8534a43e95485e6f2630e
gojoaromokudu <gojoaromokudu@gmail.com>
2020-07-04 19:22:39 +0100

    added more info to event cards

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -2,9 +2,14 @@ import React, { Component } from "react";
 
 export default class EventCard extends React.Component {
   render() {
+    const event = this.props.event;
     return (
       <div className="event-card">
-        This is a card for: {this.props.event.name}
+        <h5>This is a card for:{event.name}</h5>
+        <p>Time:{event.time}</p>
+        <p>
+          Hosted by user: {event.userId}, live from: {event.locationId}
+        </p>
       </div>
     );
   }
--- src/components/SearchResultsContainer.js
+++ src/components/SearchResultsContainer.js
@@ -2,10 +2,6 @@ import React from "react";
 import EventCard from "./EventCard";
 
 export default class EventResultsContainer extends React.Component {
-  state = {
-    // ATTRIBUTE : VALUE
-  };
-
   renderEventCards(props) {
     return props.events.map((event, idx) => (
       <EventCard event={event} key={idx} />
