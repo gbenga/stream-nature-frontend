c6019377dfdc9da4db67411782f3569866e62507
gojoaromokudu <gojoaromokudu@gmail.com>
2020-07-04 13:27:31 +0100

    made db.json with dummy data

---
+++ src/components/SearchPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class SearchPage extends React.Component {
+  state = {
+    // ATTRIBUTE : VALUE
+  };
+
+  render() {
+    return <div id="search-page">Sup</div>;
+  }
+}
