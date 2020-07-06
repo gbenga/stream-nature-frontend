ff30bfe7ff8d43ccf1164d293abba55224093479
gojoaromokudu <gojoaromokudu@gmail.com>
2020-07-06 14:20:24 +0100

    started router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import API from "./API";
 import SearchPage from "./components/SearchPage";
 import IndexContainer from "./components/IndexContainer";
 import Profile from "./components/Profile";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 
 export default class App extends Component {
   state = {
@@ -34,13 +41,21 @@ export default class App extends Component {
           <li>
             <Link to="/search">Search</Link>
           </li>
+          <li>
+            <Link to="/dummyusers">Dummy Users</Link>
+          </li>
         </ul>
+
+        <hr></hr>
         <Switch>
           <Route path="/index">
             <IndexContainer locations={this.state.locations} />
           </Route>
           <Route path="/search">
             <SearchPage events={this.state.events} />
+          </Route>
+          <Route path="/dummyusers">
+            <DummyUsers />
           </Route>
           <Route path="/">
             <Profile />
@@ -49,4 +64,29 @@ export default class App extends Component {
       </Router>
     );
   }
+}
+
+function DummyUsers() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2>This is for the dummy users</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/dummyusers`}>Dummy User 1</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path={`${match.url}/:dummyUserId`}>
+          <DummyUser />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function DummyUser() {
+  let { dummyUserId } = useParams();
+  return <div>I'm your fav {dummyUserId}</div>;
 }
