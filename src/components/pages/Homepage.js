cf9f46bff933ce2ed36754cce2fcef33523d9681
github username <github email address>
2020-07-08 21:09:11 +0100

    Moved sign-in form to sign-in page

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,36 +1,10 @@
 import React, { Component } from "react";
 
 export default class Homepage extends Component {
-  //Add this to sign in component
-state = {
-  username: "",
-  password: ""
-}
-
-//Add this function & its invokation to sign in page
-signInForm = () => {
-  return <form className="sign-in-form" onSubmit={this.handleSubmit}>
-   <input type="text" placeholder="Username"/>
-   <input type="password" placeholder="Password"/>
-   <input type="submit" value="Sign-in"/>
- </form>
- }
- 
- handleSubmit = (event) => {
-   event.preventDefault()
-   this.setState( {
-     username: event.target.value[0],
-     password: event.target.value[1]
-   } , () => console.log(this.state))
- 
- }
-
   render() {
     return (
       <div className="homepage"> 
       <h2>Hello, [insert name of user here] - how's it going?</h2>
-      {/* Add below code to sign-in page */}
-      <div> {this.signInForm()}  </div>
       </div>
     );
   }
--- src/components/pages/SignInPage.js
+++ src/components/pages/SignInPage.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
 
 export default class SignInPage extends Component {
+  
+  state = {
+    username: "",
+    password: ""
+  }
+  
+  //Add this function & its invokation to sign in page
+  signInForm = () => {
+    return <form className="sign-in-form" onSubmit={this.handleSubmit}>
+     <input type="text" placeholder="Username"/>
+     <input type="password" placeholder="Password"/>
+     <input type="submit" value="Sign-in"/>
+   </form>
+   }
+   
+   handleSubmit = (event) => {
+     event.preventDefault()
+     this.setState( {
+       username: event.target[0].value,
+       password: event.target[1].value
+     } , () => console.log(this.state))
+   
+   }
+  
   render() {
     return (
       <div className="sign-in-page">
         This is the Sign in page - add a form here
+         {/* Add below code to sign-in page */}
+      <div> {this.signInForm()}  </div>
       </div>
     );
   }
