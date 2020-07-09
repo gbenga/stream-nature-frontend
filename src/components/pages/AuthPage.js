75cc6641e51dbe93997eb036a6ab87a9cdc5438e
github username <github email address>
2020-07-09 16:10:42 +0100

    Auth complete

--- src/components/pages/AuthPage.js
+++ src/components/pages/AuthPage.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default class AuthPage extends Component {
-  
+
   render() {
     return (
       <div id="auth-page">
         This is the Auth page
-        <Link>Sign in here</Link>
+        {/* {this.props.checkLoginStatus? <button onClick={this.props.signOut}> Sign Out </button> : <Link> Sign in here</Link> } */}
         <br></br>
         <Link> Sign up here</Link>
       </div>
