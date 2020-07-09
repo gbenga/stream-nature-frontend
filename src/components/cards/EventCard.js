5255a9211b5d6c804ed7b32398183ab9063246b3
gojoaromokudu <gojoaromokudu@gmail.com>
2020-07-09 18:15:47 +0100

    event card styled

--- src/components/cards/EventCard.js
+++ src/components/cards/EventCard.js
@@ -2,6 +2,22 @@ import React, { Component } from "react";
 import API from "../../API";
 import { Card, Icon } from "semantic-ui-react";
 
+//   render() {
+//     return (
+//       <div className="event-card">
+//         <h5>This is a card for:{this.state.name}</h5>
+//         <p>Time:{this.state.time}</p>
+//         <button onClick={this.handleClickButton}>Like</button>
+//         <p>{this.state.likes} likes</p>
+//         <p>
+//           Hosted by user: {this.state.user.name}, live from:{" "}
+//           {this.state.location.city}
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+// class CardExampleCardProps extends Component {
 export default class EventCard extends Component {
   state = {
     id: 0,
@@ -25,34 +41,22 @@ export default class EventCard extends Component {
       });
     });
   };
-
   render() {
     return (
-      <div className="event-card">
-        <h5>This is a card for:{this.state.name}</h5>
-        <p>Time:{this.state.time}</p>
-        <button onClick={this.handleClickButton}>Like</button>
-        <p>{this.state.likes} likes</p>
-        <p>
-          Hosted by user: {this.state.user.name}, live from:{" "}
-          {this.state.location.city}
-        </p>
-      </div>
+      <Card
+        image={this.state.avatar}
+        header={this.state.name}
+        meta={this.state.time}
+        description={`Live from: ${this.state.location.city},${this.state.location.country}`}
+        extra={
+          <a>
+            <Icon name="user" />
+            Hosted by: {this.state.user.name}
+          </a>
+        }
+      />
     );
   }
 }
-// const CardExampleCardProps = (props) => (
-//   <Card
-//     image={props.location.avatar}
-//     header={props.location.city}
-//     meta={props.location.country}
-//     description={props.location.bio}
-//     extra={
-//       <a>
-//         <Icon name="map pin" />
-//       </a>
-//     }
-//   />
-// );
 
 // export default CardExampleCardProps;
