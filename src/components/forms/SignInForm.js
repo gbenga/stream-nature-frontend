47e49af9e3c455735a31c5b287da1d535a0d9da4
gojoaromokudu <gojoaromokudu@gmail.com>
2020-08-18 17:27:28 +0100

    Refactor sign in form

--- src/components/forms/SignInForm.js
+++ src/components/forms/SignInForm.js
@@ -1,24 +1,53 @@
 import React, { Component } from "react";
+import { Form, Button } from "semantic-ui-react";
+import API from "../../API";
 
 export default class SignInForm extends Component {
+  state = {
+    username: "",
+    password: "",
+  };
+
+  handleUsernameChange = (e) => {
+    this.setState({ username: e.target.value });
+  };
+
+  handlePasswordChange = (e) => {
+    this.setState({ password: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.sendSignInDataToRails();
+    API.signIn(this.state)
+      .then((json) => this.props.signIn(json.user, json.token))
+      .then(
+        this.setState({
+          username: "",
+          password: "",
+        })
+      );
+  };
+
   render() {
-    return;
-    <Form className="sign-in-form" onSubmit={this.handleSubmit}>
-      <Form.Field required>
-        <Form.Input
-          onChange={this.handleUsernameChange}
-          type="text"
-          placeholder="Username"
-          label="Username"
-        />
-        <Form.Input
-          onChange={this.handlePasswordChange}
-          type="password"
-          placeholder="Password"
-          label="Password"
-        />
-        <Button content="Sign-in" value="Sign-in" />
-      </Form.Field>
-    </Form>;
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Field required>
+          <Form.Input
+            onChange={this.handleUsernameChange}
+            type="text"
+            placeholder="Username"
+            label="Username"
+          />
+          <Form.Input
+            onChange={this.handlePasswordChange}
+            type="password"
+            placeholder="Password"
+            label="Password"
+          />
+          <Button content="Sign-in" value="Sign-in" />
+        </Form.Field>
+      </Form>
+    );
   }
 }
--- src/components/pages/SignInPage.js
+++ src/components/pages/SignInPage.js
@@ -1,51 +1,8 @@
 import React, { Component } from "react";
-import { Card, Icon, Form, Button } from "semantic-ui-react";
-import signInForm from "../forms/SignInForm";
+import { Card, Icon } from "semantic-ui-react";
 import SignInForm from "../forms/SignInForm";
 
 export default class SignInPage extends Component {
-  state = {
-    username: "",
-    password: "",
-  };
-
-  handleUsernameChange = (event) => {
-    this.setState({ username: event.target.value });
-  };
-
-  handlePasswordChange = (event) => {
-    this.setState({ password: event.target.value });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.sendSignInDataToRails();
-  };
-
-  //----------- (POST) Send SignIn Data from state To Rails -----------//
-  sendSignInDataToRails = () => {
-    let configObj = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(this.state), //We send the state as the body of the configObj
-    };
-
-    return fetch("http://localhost:3000/api/v1/sign-in", configObj)
-      .then((resp) => resp.json())
-      .then((jso) => {
-        if (jso.error) {
-          // handleError
-        } else {
-          this.props.signIn(jso.user, jso.token);
-          this.props.history.push(`/users/${jso.user.id}`);
-        }
-      })
-      .catch((error) => console.log(error.message));
-  };
-
   render() {
     return (
       <div className="sign-in-page">
